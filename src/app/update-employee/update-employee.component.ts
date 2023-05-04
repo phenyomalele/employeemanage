b59a44406ac06b52f4a68380972c9049185af062
phenyomalele <phenyomalele@gmail.com>
2023-05-04 18:14:24 +0200

    first commit

---
+++ src/app/update-employee/update-employee.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-update-employee',
+  templateUrl: './update-employee.component.html',
+  styleUrls: ['./update-employee.component.css']
+})
+export class UpdateEmployeeComponent implements OnInit {
+
+  employee_Id: number;
+  employee: Employee = new Employee();
+  constructor(private employeeService: EmployeeService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+    ngOnInit(): void {
+      this.employee_Id = this.route.snapshot.params['id'];
+  
+      this.employeeService.getEmployeeById(this.employee_Id).subscribe(data => {
+        this.employee = data;
+      }, error => console.log(error));
+    }
+
+    onSubmit(){
+      
+      this.employeeService.updateEmployee(this.employee_Id, this.employee).subscribe((data:any) =>{
+        
+        this.goToEmployeeList();
+     
+      }
+      , error => console.log(error));
+    }
+
+    goToEmployeeList(){
+      this.router.navigate(['/employees']);
+    }
+}
